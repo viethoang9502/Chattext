f0b996d6f7390e7e7fd8c95b1fff8e086089ab90
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-05-22 16:56:05 +0700

    Update ClientThread.java

--- src/sendfile/client/ClientThread.java
+++ src/sendfile/client/ClientThread.java
@@ -11,16 +11,7 @@ import java.util.StringTokenizer;
 import java.util.Vector;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author hanhhoatranthi
- */
 public class ClientThread implements Runnable{
     
     Socket socket;
