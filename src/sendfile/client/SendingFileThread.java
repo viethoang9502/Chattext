90d4e08f54d31f8d730e7539e6b037358b678589
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-05-22 16:55:01 +0700

    Update SendingFileThread.java

--- src/sendfile/client/SendingFileThread.java
+++ src/sendfile/client/SendingFileThread.java
@@ -13,16 +13,7 @@ import java.net.Socket;
 import java.text.DecimalFormat;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author hanhhoatranthi
- */
 public class SendingFileThread implements Runnable {
     
     protected Socket socket;
