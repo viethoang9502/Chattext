34fc36d559189edb5b8cb72ba76fc9ea3eb9ab61
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-05-22 16:52:27 +0700

    Update ServerThread.java

--- src/sendfile/server/ServerThread.java
+++ src/sendfile/server/ServerThread.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package sendfile.server;
 
@@ -10,10 +5,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-/**
- *
- * @author hanhhoatranthi
- */
+
 public class ServerThread implements Runnable {
     
     ServerSocket server;
