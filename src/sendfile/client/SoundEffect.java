929659de7a0165cf1f865d50677f54e90b1b22fe
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-05-22 16:55:19 +0700

    Update SoundEffect.java

--- src/sendfile/client/SoundEffect.java
+++ src/sendfile/client/SoundEffect.java
@@ -9,16 +9,6 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author hanhhoatranthi
- */
 public enum SoundEffect {
     
     MessageReceive("/audio/Ring.wav", false), //  Ringtone for Chat message receive
