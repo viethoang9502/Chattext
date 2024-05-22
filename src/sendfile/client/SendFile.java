d2b94d9206dc985048a781eaf6e48e3d204ebc95
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-05-22 16:54:39 +0700

    Update SendFile.java

--- src/sendfile/client/SendFile.java
+++ src/sendfile/client/SendFile.java
@@ -11,16 +11,6 @@ import java.util.StringTokenizer;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author hanhhoatranthi
- */
 
 public class SendFile extends javax.swing.JFrame {
 
