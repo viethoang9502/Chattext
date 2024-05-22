1e137e15f6b1ad370cf4637066c4b99e8536efa3
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-05-22 16:54:29 +0700

    Update MessageStyle.java

--- src/sendfile/client/MessageStyle.java
+++ src/sendfile/client/MessageStyle.java
@@ -7,16 +7,7 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyleContext;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author hanhhoatranthi
- */
 public class MessageStyle {
     
     
