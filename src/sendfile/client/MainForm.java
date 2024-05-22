a014f6ec0e9784c07432d1fa6ff6964d238eb998
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-05-22 16:56:26 +0700

    Update MainForm.java

--- src/sendfile/client/MainForm.java
+++ src/sendfile/client/MainForm.java
@@ -19,16 +19,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author hanhhoatranthi
- */
 public class MainForm extends javax.swing.JFrame {
     String username;
     String host;
